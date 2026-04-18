8db09c9d59e248eb4a53aaa14df45fa3ebe03781
ajayrabidas <ajayrabidas@gmail.com>
2026-04-18 18:01:51 +0530

    Product Service CRUD Operation Demon using Spring Boot 4.0.x and REST API

---
+++ product-service/src/main/java/com/crud/demo/ProductService.java
@@ -0,0 +1,31 @@
+package com.crud.demo;
+
+import com.crud.demo.entity.Product;
+import com.crud.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+// TODO: to do the rest of the operation
+
+}
---
+++ product-service/src/test/java/com/crud/demo/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.demo.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
